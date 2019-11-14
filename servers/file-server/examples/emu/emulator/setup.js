e392c7730670fa7e3531a625fab48c48c21f2807
Paul Sori <paul.sori@gmail.com>
2019-11-14 13:48:15 -0500

    more safari workarounds

--- servers/file-server/examples/emu/emulator/setup.js
+++ servers/file-server/examples/emu/emulator/setup.js
@@ -98,10 +98,12 @@ function nes_init(canvas_id){
 	framebuffer_u32 = new Uint32Array(buffer);
 	
 	// Setup audio.
-	var audio_ctx = new window.AudioContext();
-	var script_processor = audio_ctx.createScriptProcessor(AUDIO_BUFFERING, 0, 2);
-	script_processor.onaudioprocess = audio_callback;
-	script_processor.connect(audio_ctx.destination);
+	if (window.AudioContext ) {
+		var audio_ctx = new window.AudioContext();
+		var script_processor = audio_ctx.createScriptProcessor(AUDIO_BUFFERING, 0, 2);
+		script_processor.onaudioprocess = audio_callback;
+		script_processor.connect(audio_ctx.destination);
+	}
 }
 
 function nes_boot(rom_data){
